40e9ba8c9b77f255866decd90e8ce7b606f851f6
luish <luish@DESKTOP-JAP4ND1>
2019-09-25 22:18:53 -0300

    "Atividade 06"

---
+++ Atividade06/src/java/servlets/LoginServlet.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+* @author luish
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Sistema Acadêmico</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Efetuar Login</h1>");
+            out.println("<form action=\"/Atividade06/login\" method=\"post\">");
+            out.println("<div>");
+            out.println("<label>Matrícula:</label>");
+            out.println("<input type=\"text\" name=\"matricula\" size=\"15\" value=\"\">");
+            out.println("</div>");
+            out.println("<div>");
+            out.println("<label>Senha:</label>");
+            out.println("<input type=\"password\" name=\"senha\" size=\"15\" value=\"\">");
+            out.println("</div>");
+            out.println("<input type=\"submit\" value=\"Entrar\"/>");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String matricula = request.getParameter("matricula");
+        String senha = request.getParameter("senha");
+
+        if (matricula == null || senha == null) {
+            response.sendRedirect(request.getContextPath() + "/login");
+        } else {
+            HttpSession sessao = request.getSession();
+            sessao.setAttribute("autenticado", true);
+            response.sendRedirect(request.getContextPath() + "/");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
